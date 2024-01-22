7c764709dcf97d602076a2757cf5883f31ed0a1c
User <phillipdang@192.168.1.9>
2024-01-22 11:43:11 +0700

    Khoi tao du an Monday cua khoa hoc CodeWithMentor

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <h1>Monday project</h1>
+    </BrowserRouter>
+  );
+}
+
+export default App;
